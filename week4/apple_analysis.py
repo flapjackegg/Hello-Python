ba35a67a14f91f7b1c0fba595c5cd692ba94091d
hammer <tianhanming11@gmail.com>
2017-12-09 18:02:56 +0800

    challenge 15

---
+++ week4/apple_analysis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+
+def quarter_volume():
+    try:
+        data = pd.read_csv('apple.csv', header=0)
+    except ValueError:
+        return 0, 0
+    newdata = data.Volume
+    newdata.index = pd.to_datetime(data.Date)
+    season_volume = newdata.resample('3m').sum().shift(2, freq='m')
+    second_volume = season_volume.sort_values()[-2]
+    return second_volume
+
+
+if __name__ == '__main__':
+    print(quarter_volume())
