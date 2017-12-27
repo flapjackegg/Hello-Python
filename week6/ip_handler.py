432a7d7d9f603faf1bc77176978954fa2d273b40
hammer <tianhanming11@gmail.com>
2017-12-27 16:20:28 +0800

    challenge 20

---
+++ week6/ip_handler.py
@@ -0,0 +1,44 @@
+import re
+from wechatpy.messages import TextMessage
+from wechatpy import create_reply
+from qqwry import QQwry
+
+
+class CommandHandler:
+    command = ''
+
+    def check_match(self, message):
+        if not isinstance(message, TextMessage):
+            return False
+
+        if not message.content.strip().lower().startswith(self.command):
+            return False
+        return True
+
+
+class IpLocationHandler(CommandHandler):
+    command = 'ip'
+
+    def __init__(self):
+        self.qqwry = QQwry()
+        self.qqwry.load_file('qqwry.dats')
+        self.msg = ''
+
+    def handler(self, message):
+        if not self.check_match(message):
+            return
+
+        ip = message.strip()
+        pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'
+
+        if not re.match(pattern, ip):
+            msg = 'IP地址无效'
+        
+        res = self.qqwry.lookup(ip)
+
+        if res is None:
+            msg = '未找到'
+        else:
+            msg = res[0]
+        return create_reply(msg, message)
+
