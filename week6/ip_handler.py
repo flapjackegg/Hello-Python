f722db70dec98712d977ec6ab546855e5dafb0fc
hammer <tianhanming11@gmail.com>
2017-12-28 10:25:14 +0800

    challenge 20

--- week6/ip_handler.py
+++ week6/ip_handler.py
@@ -1,44 +1,42 @@
 import re
+import os
 from wechatpy.messages import TextMessage
 from wechatpy import create_reply
 from qqwry import QQwry
-
 
 class CommandHandler:
     command = ''
 
     def check_match(self, message):
+        """检查消息是否匹配命令模式
+        """
         if not isinstance(message, TextMessage):
             return False
-
         if not message.content.strip().lower().startswith(self.command):
             return False
         return True
 
 
-class IpLocationHandler(CommandHandler):
+class IPLocationHandler(CommandHandler):
     command = 'ip'
 
     def __init__(self):
-        self.qqwry = QQwry()
-        self.qqwry.load_file('qqwry.dats')
-        self.msg = ''
+        file = os.environ.get('QQWRY_DAT', 'qqwry.dat')
+        self.q = QQwry()
+        self.q.load_file(file)
 
-    def handler(self, message):
+    def handle(self, message):
         if not self.check_match(message):
             return
-
-        ip = message.content.strip()
+        parts = message.content.strip().split()
+        if len(parts) == 1 or len(parts) > 2:
+            return create_reply('IP地址无效', message)
+        ip = parts[1]
         pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'
-
         if not re.match(pattern, ip):
-            self.msg = 'IP地址无效'
-        
-        res = self.qqwry.lookup(ip)
-
-        if res is None:
-            self.msg = '未找到'
+            return create_reply('IP地址无效', message)
+        result = self.q.lookup(ip)
+        if result is None:
+            return create_reply('未找到', message)
         else:
-            self.msg = res[0]
-        return create_reply(self.msg, message)
-
+            return create_reply(result[0], message)
